7cb496b39b75c67c165662dc1f7e20db85683b99
IvanPrut <ivanprut@list.ru>
2024-11-12 17:26:25 +0300

    added basic functionality

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,15 @@
 package koschei.models;
 
 public class Deth8 {
+
+    private Deth8(){}
+
+    public static Deth8 getDeth8(){
+        return new Deth8();
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
